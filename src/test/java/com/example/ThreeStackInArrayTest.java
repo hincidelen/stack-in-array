5d9486ec838012cf794252c72a58530c460063b6
haluk.incidelen <Hinci2012>
2021-01-19 00:24:46 +0300

    test import fix

--- src/test/java/com/example/ThreeStackInArrayTest.java
+++ src/test/java/com/example/ThreeStackInArrayTest.java
@@ -1,20 +1,22 @@
 package com.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Stack;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ThreeStackInArrayTest {
 
 	@Test
 	public void testThreeStackInArray() {
+
 		Stack[] arrayWithThreeStack = ThreeStackInArray.createArrayWithThreeStack();
 		assertNotNull(arrayWithThreeStack);
 		assertEquals(3, arrayWithThreeStack.length);
+
 		assertTrue(arrayWithThreeStack[0] instanceof Stack);
 		assertTrue(arrayWithThreeStack[1] instanceof Stack);
 		assertTrue(arrayWithThreeStack[2] instanceof Stack);
